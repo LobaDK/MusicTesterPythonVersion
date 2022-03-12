aad75182f5f319ca563b008785fd0ddbf7472037
LobaDK <lobadk@lobadk.com>
2022-03-13 00:51:34 +0100

    added code to display files done and left.

--- MusicTesterPythonVersion/MusicTesterPythonVersion.py
+++ MusicTesterPythonVersion/MusicTesterPythonVersion.py
@@ -5,16 +5,22 @@ import sqlite3
 con = sqlite3.connect(r'C:\Users\nickl\Music\Distance Music\AllowedSongs.db')
 files = os.listdir(r'C:\Users\nickl\Music\Distance Music')
 c = con.cursor()
+fd = 0
+fl = os.listdir(r'C:\Users\nickl\Music\Distance Music')
 for file in files:
     file = 'C:\\Users\\nickl\\Music\\Distance Music\\' + file
+    fd = fd + 1
+    
     
     ID = c.execute('SELECT SongID FROM Songs WHERE Path == ?', (file,)).fetchall()
     if len(ID) == 0:
         cmd = ['ffplay',file]
         subprocess.Popen(cmd).wait()
+        print(f'File {fd} out of {len(fl)} files')
         FileDelete = input("Delete file?")
         if FileDelete == 'y':
             os.remove(file)
+            fl = os.listdir(r'C:\Users\nickl\Music\Distance Music')
         else:
             c.execute('INSERT INTO Songs (Path) VALUES (?)', (file,))
             con.commit()
