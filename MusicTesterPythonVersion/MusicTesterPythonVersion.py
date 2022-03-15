a0a58ee0acd4b3263a1742a09b76c85849861f40
LobaDK <lobadk@lobadk.com>
2022-03-15 22:47:57 +0100

    Added percentage display

--- MusicTesterPythonVersion/MusicTesterPythonVersion.py
+++ MusicTesterPythonVersion/MusicTesterPythonVersion.py
@@ -5,8 +5,10 @@ import sqlite3
 con = sqlite3.connect(r'C:\Users\nickl\Music\Distance Music\AllowedSongs.db')
 files = os.listdir(r'C:\Users\nickl\Music\Distance Music')
 c = con.cursor()
-fd = 0
+fd = int(0)
+fl = int(0)
 fl = os.listdir(r'C:\Users\nickl\Music\Distance Music')
+fl = len(fl)
 for file in files:
     file = 'C:\\Users\\nickl\\Music\\Distance Music\\' + file
     fd = fd + 1
@@ -16,11 +18,16 @@ for file in files:
     if len(ID) == 0:
         cmd = ['ffplay',file]
         subprocess.Popen(cmd).wait()
-        print(f'File {fd} out of {len(fl)} files')
+        fdiv = fd / fl 
+        fmul = fdiv * 100
+        fper = round(fmul,2)
+        print(f'File {fd} out of {fl} files, or {fper}% done')
         FileDelete = input("Delete file?")
         if FileDelete == 'y':
             os.remove(file)
             fl = os.listdir(r'C:\Users\nickl\Music\Distance Music')
+            fl = len(fl)
+            fd = fd - 1
         else:
             c.execute('INSERT INTO Songs (Path) VALUES (?)', (file,))
             con.commit()
