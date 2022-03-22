30e8f37a731c0268ffb639decb6038eece5a8679
LobaDK <lobadk@lobadk.com>
2022-03-22 11:21:01 +0100

    Added code to save in the database if the song got deleted or kept

--- MusicTesterPythonVersion/MusicTesterPythonVersion.py
+++ MusicTesterPythonVersion/MusicTesterPythonVersion.py
@@ -13,7 +13,13 @@ for file in files:
     file = 'C:\\Users\\nickl\\Music\\Distance Music\\' + file
     fd = fd + 1
     
-    
+    delfile = c.execute('SELECT Deleted FROM Songs WHERE Path == ?', (file,)).fetchall()
+    try:
+        if delfile[0][0] == 1:
+            os.remove(file)
+            continue
+    except IndexError:
+        pass
     ID = c.execute('SELECT SongID FROM Songs WHERE Path == ?', (file,)).fetchall()
     if len(ID) == 0:
         cmd = ['ffplay',file]
@@ -24,10 +30,14 @@ for file in files:
         print(f'File {fd} out of {fl} files, or {fper}% done')
         FileDelete = input("Delete file?")
         if FileDelete == 'y':
+            c.execute('INSERT INTO Songs (Path) VALUES (?)', (file,))
+            c.execute('UPDATE Songs SET Deleted = 1 WHERE Path == ?', (file,))
+            con.commit()
             os.remove(file)
             fl = os.listdir(r'C:\Users\nickl\Music\Distance Music')
             fl = len(fl)
             fd = fd - 1
         else:
             c.execute('INSERT INTO Songs (Path) VALUES (?)', (file,))
+            c.execute('UPDATE Songs SET Deleted = 0 WHERE Path == ?', (file,))
             con.commit()
