7d992a15c935cdc957bb92896863f21e975279ff
Nicklas H. <gta5.r6s.bf4@gmail.com>
2023-11-13 11:08:07 +0100

    Finished version 2

--- MusicTesterPythonVersion2.py
+++ MusicTesterPythonVersion2.py
@@ -1,42 +1,29 @@
 from os import remove, mkdir, listdir
-from subprocess import Run
+from subprocess import run
 import sqlite3
 from pathlib import Path
 from tqdm import tqdm
 from shutil import copy2
 
-database_path = r'C:\Users\nichel\Music\FavoriteMusic\AllowedSongs.db'
-
-music_path = r'C:\Users\nichel\Music\FavoriteMusic'
-
-output_path = r'C:\Users\nichel\Music\AllowedMusic'
-
+# Check if ffmpeg is installed either in the system or in the same folder as this script
 try:
-    mkdir(output_path)
-except FileExistsError:
-    pass
+    run(['ffplay', '-version'])
+except FileNotFoundError:
+    try:
+        run([str(Path(Path(__file__).parent, 'ffplay.exe')), '-version'])
+    except FileNotFoundError:
+        print('ffmpeg not found. Please install it and add it to your PATH variable or the folder this script is in.')
+        print('You can download it here: https://ffmpeg.org/download.html. Press enter to exit...')
+        exit()
 
-# Connect or create database
-con = sqlite3.connect(database_path)
 
-# Create table if it doesn't exist
-c = con.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS Songs
-                (SongID INTEGER PRIMARY KEY AUTOINCREMENT, Path TEXT, Allowed INTEGER)''')
-con.commit()
-
-# Get list of m4a files in folder
-files = [file for file in listdir(music_path) if file.endswith('.m4a')]
-
-# Create progress bar
-pbar = tqdm(total=len(files), desc='Files', unit='files', position=2)
-
-# Iterate through files
-for file in files:
-    full_path = str(Path(music_path, file))
-    allowed = c.execute('SELECT Allowed FROM Songs WHERE Path == ?', (full_path,)).fetchall()
-
-    # Check if the file is in the database and marked as allowed
+def StartupCheck(full_path, allowed, output_path, file):
+    """
+    Checks if the file is in the database and marked as allowed.
+    If the file is marked as allowed and does not exist in the output path, it is copied to the output path.
+    If the file is marked as not allowed and exists in the output path, it is removed from the output path.
+    Returns True if the file was copied or removed, False otherwise.
+    """
     try:
         # Database returns a list of tuples.
         # This means that allowed[0] is the first tuple in the list
@@ -52,17 +39,76 @@ for file in files:
         elif allowed[0][0] == 0:
             if Path(output_path, file).exists():
                 remove(str(Path(output_path, file)))
-
+        return True
     # If the file is not in the database
-    # moved is empty and an IndexError is raised
+    # allowed will be empty and an IndexError is raised
     except IndexError:
         pass
+    return False
 
-    # Play file
+
+# Ask for database path. If no path is given, use the default path
+database_path = input('Database path: ') or str(Path(Path(__file__).parent, 'MusicTester.db'))
+
+# Ask for music folder path. If no path is given, use the default path
+music_path = input('Music folder path: ') or str(Path(Path(__file__).parent, 'Music'))
+
+# Ask for output folder path. If no path is given, use the default path
+output_path = input('Output folder path: ') or str(Path(Path(__file__).parent, 'Output'))
+
+try:
+    mkdir(output_path)
+except FileExistsError:
+    pass
+
+# Connect or create database
+con = sqlite3.connect(database_path)
+
+# Create table if it doesn't exist and make Path case insensitive
+c = con.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS Songs
+                (SongID INTEGER PRIMARY KEY AUTOINCREMENT, Path TEXT COLLATE NOCASE, Allowed INTEGER)''')
+con.commit()
+
+# Get list of m4a files in folder
+try:
+    files = [file for file in listdir(music_path) if file.endswith('.m4a')]
+except FileNotFoundError:
+    print('Music folder not found. Please create a folder called Music in the same folder as this script and put your music files in it. Press enter to exit...')
+    exit()
+
+if len(files) == 0:
+    print('No music files found. Please put your music files in the Music folder. Press enter to exit...')
+    exit()
+
+# Create progress bar
+pbar = tqdm(total=len(files), desc='Files', unit='files', position=2)
+
+# Iterate through files
+for file in files:
+    full_path = str(Path(music_path, file))
+    allowed = c.execute('SELECT Allowed FROM Songs WHERE Path == ?', (full_path,)).fetchall()
+
+    pbar.update(1)
+
+    # Use StartupCheck to check if any any files need to be copied to or deleted in the output folder
+    if StartupCheck(full_path, allowed, output_path, file):
+        continue
+
+    # Play the audio file
     cmd = ['ffplay', full_path]
-    Run(cmd)
+    run(cmd)
 
     # Ask if the file should be allowed
     allowed_input = ''
     while allowed_input.casefold() not in ['y', 'n']:
-        allowed_input = input('Allow file? (y/n): ').casefold()
+        pbar.refresh()
+        allowed_input = input('\nAllow file? (y/n): ').casefold()
+
+    # Add file to database
+    c.execute('INSERT INTO Songs (Path, Allowed) VALUES (?, ?)', (full_path, 1 if allowed_input == 'y' else 0))
+    con.commit()
+
+    # Copy file to output folder if allowed
+    if allowed_input == 'y':
+        copy2(full_path, str(Path(output_path, file)))
